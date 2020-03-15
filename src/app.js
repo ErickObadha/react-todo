cba0d694c75b509e6ee44063e39276ba8b4cc314
ErickObadha <erickobadha@gmail.com>
2020-03-15 13:33:18 +0300

    react todo

---
+++ src/app.js
@@ -0,0 +1,165 @@
+class TodoApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+		this.handlePick = this.handlePick.bind(this);
+		this.handleAddOption = this.handleAddOption.bind(this);
+		this.handleDeleteOption = this.handleDeleteOption.bind(this)
+		this.state = {
+			options: []
+		}
+	}
+	handleDeleteOptions() {
+		this.setState(() => {
+			return {
+				options: []
+			}
+		})
+	}
+	handleDeleteOption(optionToRemove) {
+		this.setState((prevState) => {
+			return {
+				options: prevState.options.filter((option) => optionToRemove !== option)
+			}
+		})
+	}
+	handlePick() {
+		const random = Math.floor(Math.random() * 
+			this.state.options.length);
+		const option = this.state.options[random];
+		alert(option);
+	}
+	handleAddOption(option) {
+		if(!option) {
+			return 'Enter valid value to add item';
+		} else if (this.state.options.indexOf(option) > -1) {
+			return 'This option already exists';
+		}
+		this.setState((prevState) => { 
+			return {
+				options: prevState.options.concat(option)
+			}
+		})
+	}
+	ComponentDidUpdate(prevProps, prevState) {
+		if (prevState.options.length != this.state.options.length) {
+			const json = JSON.stringify(this.state.options);
+			localStorage.setItem('options', json); 
+		}
+		console.log(data.options)
+	}
+	ComponentDidMount() {
+		try {
+			const json = localStorage.getItem(options);
+			const parse = JSON.parse(json);
+			if (options) {
+				this.setState(() => {
+					return { options }
+				})
+			}
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	ComponentDidMount() {
+		
+	}
+	render() {
+		const title ='TodoApp';
+		const subtitle = 'Organize schedule';
+		return (
+			<div>
+			<Header title={title} subtitle={subtitle} />
+			<Action 
+			handlePick={this.handlePick}
+			hasOptions={this.state.options.length > 0}
+			/>
+			<Options 
+			options={this.state.options} 
+			handleDeleteOptions={this.handleDeleteOptions}
+			handleDeleteOption={this.handleDeleteOption}
+			/>
+			<Addoption 
+			handleAddOption={this.handleAddOption}
+			/>
+			</div>
+			)
+	}
+}
+
+const Header = (props) => (
+	<div>
+       <h1>{props.title}</h1>
+       <h2>{props.subtitle}</h2>
+	</div>
+	)
+class Action extends React.Component {
+		render () {
+			return (
+		<div>
+		<button 
+		onClick={this.props.handlePick}
+		disabled={!this.props.hasOptions}>
+		What should I do
+		</button>
+		</div>
+		)
+	}
+}
+class Options extends React.Component {
+	constructor(props){
+	super(props);
+}
+	render () {
+		return(
+		<div>
+			<button onClick={this.props.handleDeleteOptions}>Remove All</button>
+			{this.props.options.map((option) => {
+		return (
+		<Option 
+		key={option} 
+		optionText={option} 
+		handleDeleteOption={this.props.handleDeleteOption}
+		/>
+		);
+	})}</div>)
+		}
+	}
+	const Option = (props) => (
+	<div>
+	{props.optionText}
+	<button onClick={() => {props.handleDeleteOption(props.optionText)}}
+	>remove</button>
+	</div>
+	)
+	class Addoption extends React.Component {
+		constructor(props) {
+			super(props);
+			this.handleAddOption = this.handleAddOption.bind(this);
+			this.state ={
+				error: undefined
+			}
+		}
+		handleAddOption(e) {
+			e.preventDefault();
+			const option = e.target.elements.option.value.trim();
+			const error = this.props.handleAddOption(option);
+			this.setState(() => {
+				return { error }
+			})
+			e.target.elements.option.value =''
+		}
+		render() {
+			return (
+				<div>
+				{this.state.error && <p>{this.state.error}</p>}
+				<form onSubmit={this.handleAddOption}>
+				<input type="text" name="option" />
+				<button>Add Option</button>
+				</form>
+				</div>
+		)
+	  }
+	}
+
+ReactDOM.render(<TodoApp />, document.getElementById('app'));
